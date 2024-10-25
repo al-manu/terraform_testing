85b78f93c05fbd3a71d63d130cf83be92b6b6830
Ala Mani <ala.a.mani@capgemini.com>
2024-10-25 11:22:18 +0100

    add workflow directory

---
+++ aws_infra_test/README.md
@@ -0,0 +1,15 @@
+my-aws-infra/
+├── .github/
+│   └── workflows/
+│       └── terraform-deploy.yml
+├── env/
+│   └── dev.tfvars
+├── modules/
+│   └── vpc/
+│       ├── main.tf
+│       ├── variables.tf
+│       └── outputs.tf
+├── main.tf
+├── variables.tf
+├── outputs.tf
+└── README.md
---
+++ aws_infra_test/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
