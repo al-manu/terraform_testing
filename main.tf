a2664aeaf43ec9966dd7f65f812d2bfd96a798ad
Ala Mani <ala.a.mani@capgemini.com>
2024-10-25 14:36:56 +0100

    fix workflow structure c

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
+# provider "aws" {
+#   region = var.aws_region
+# }
+
+# module "vpc" {
+#   source = "./modules/vpc"
+#   vpc_name = var.vpc_name
+#   cidr_block = var.cidr_block
+# }
+
+
 provider "aws" {
   region = var.aws_region
 }
 
-module "vpc" {
-  source = "./modules/vpc"
-  vpc_name = var.vpc_name
-  cidr_block = var.cidr_block
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,34 @@
+# variable "aws_region" {
+#   description = "AWS region"
+#   type        = string
+#   default     = "eu-central-1"
+# }
+
+# variable "vpc_name" {
+#   description = "Name of the VPC"
+#   type        = string
+# }
+
+# variable "cidr_block" {
+#   description = "CIDR block for the VPC"
+#   type        = string
+#   default     = "10.0.0.0/16"
+# }
+
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "eu-central-1"
-}
-
-variable "vpc_name" {
-  description = "Name of the VPC"
+  description = "The AWS region to create resources in"
   type        = string
 }
 
-variable "cidr_block" {
-  description = "CIDR block for the VPC"
+variable "environment" {
+  description = "The environment (e.g., dev, prod)"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "dev"
 }
+
