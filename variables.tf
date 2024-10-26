dce48211b66cacb2151e13fc3ddca19d2885c394
Ala Mani <ala.a.mani@capgemini.com>
2024-10-26 10:43:55 +0100

    fix workflow structure 000

--- variables.tf
+++ variables.tf
@@ -1,21 +1,3 @@
-# variable "aws_region" {
-#   description = "AWS region"
-#   type        = string
-#   default     = "eu-central-1"
-# }
-
-# variable "vpc_name" {
-#   description = "Name of the VPC"
-#   type        = string
-# }
-
-# variable "cidr_block" {
-#   description = "CIDR block for the VPC"
-#   type        = string
-#   default     = "10.0.0.0/16"
-# }
-
-
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
