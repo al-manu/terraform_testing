a263aba2b2b712a01d53503d9941ccfdb6a2eb43
Ala Mani <ala.a.mani@capgemini.com>
2024-10-26 10:51:07 +0100

    fix workflow structure 007

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
-  acl    = "private"
 
   tags = {
     Name        = var.bucket_name
     Environment = var.environment
   }
 }
+
