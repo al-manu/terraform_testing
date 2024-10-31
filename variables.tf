3a48c03e38dd5bda2ea0e7e430f000768ef6f812
Ala Mani <ala.a.mani@capgemini.com>
2024-10-31 10:25:41 +0100

    add lambda function

--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "aws_region" {
   description = "The AWS region to deploy resources"  # Description of the variable
   type        = string  # The type of variable
 }
+variable "aws_role_arn" {
+  description = "The ARN of the role to assume"
+  type        = string
+}
