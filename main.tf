bb348da0ae2915e78c5be028cba88933ac791b7e
Ala Mani <ala.a.mani@capgemini.com>
2024-10-31 11:21:23 +0100

    add lambda function

--- main.tf
+++ main.tf
@@ -2,27 +2,40 @@ provider "aws" {
   region = var.aws_region  # Set AWS region from variable
 }
 
-# Data source to get the IAM role by ARN
-data "aws_iam_role" "existing_role" {
-  arn = var.aws_role_arn  # Fetch the role by ARN
+# Create an IAM role for the Lambda function
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_execution_role"  # Name of the IAM role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"  # Allows Lambda to assume this role
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      },
+    ]
+  })
 }
 
-# Attach the basic execution policy to the existing role
+# Attach the basic execution policy to the role
 resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
   name       = "lambda_policy_attachment"
-  roles      = [data.aws_iam_role.existing_role.name]  # Attach to the existing IAM role
+  roles      = [aws_iam_role.lambda_role.name]  # Attach to the created IAM role
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Define the Lambda function using the existing role
+# Define the Lambda function using the newly created role
 resource "aws_lambda_function" "my_lambda" {
-  function_name = "myBasicLambda"
-  role          = data.aws_iam_role.existing_role.arn  # Use the existing IAM role ARN
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
-  timeout       = 3
+  function_name = "myBasicLambda"  # Name of the Lambda function
+  role          = aws_iam_role.lambda_role.arn  # ARN of the IAM role
+  handler       = "lambda_function.lambda_handler"  # The handler function to execute
+  runtime       = "python3.8"  # Runtime environment for the Lambda function
+  timeout       = 3  # Timeout in seconds
   
   # Specify the code for the Lambda function
-  filename         = "lambda_function.zip"
-  source_code_hash = filebase64sha256("lambda_function.zip")
+  filename         = "lambda_function.zip"  # Path to the zip file containing the Lambda code
+  source_code_hash = filebase64sha256("lambda_function.zip")  # Hash of the zip file for change detection
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
+output "lambda_function_name" {
+  description = "The name of the created Lambda function"
+  value       = aws_lambda_function.my_lambda.function_name
+}
+
 output "lambda_function_arn" {
-  description = "The ARN of the Lambda function"
-  value       = aws_lambda_function.my_lambda.arn  # Outputs the ARN of the created Lambda function
+  description = "The ARN of the created Lambda function"
+  value       = aws_lambda_function.my_lambda.arn
 }
 
 output "lambda_role_arn" {
   description = "The ARN of the IAM role used by the Lambda function"
-  value       = aws_iam_role.lambda_role.arn  # Outputs the ARN of the IAM role
+  value       = data.aws_iam_role.existing_role.arn
 }
