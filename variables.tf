623eb9bd07ec11fccbff839c55f0c732cc96abf2
Ala Mani <ala.a.mani@capgemini.com>
2024-11-01 16:59:23 +0100

    fix state s3 lambda

--- variables.tf
+++ variables.tf
@@ -10,23 +10,23 @@ variable "TF_STATE_BUCKET" {
 variable "in_bucket_name" {
   description = "Name of the input S3 bucket"
   type        = string
-  default     = "test-in-oct241"  # Change this as needed for your environment
+  default     = "test-in-oct2401"  # Change this as needed for your environment
 }
 
 variable "out_bucket_name" {
   description = "Name of the output S3 bucket"
   type        = string
-  default     = "test-out-oct241"  # Change this as needed for your environment
+  default     = "test-out-oct2401"  # Change this as needed for your environment
 }
 
 variable "tmp_bucket_name" {
   description = "Name of the temporary S3 bucket"
   type        = string
-  default     = "test-tmp-oct241"  # Change this as needed for your environment
+  default     = "test-tmp-oct2401"  # Change this as needed for your environment
 }
 
 variable "export_bucket_name" {
   description = "Name of the export S3 bucket"
   type        = string
-  default     = "test-export-oct241"  # Change this as needed for your environment
+  default     = "test-export-oct2401"  # Change this as needed for your environment
 }
