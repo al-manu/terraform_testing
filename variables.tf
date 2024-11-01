589ecb422760213a3d89a16f2a3695a81c873913
Ala Mani <ala.a.mani@capgemini.com>
2024-11-01 14:40:12 +0100

    fix state v3

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "TF_STATE_BUCKET" {
   description = "The name of the S3 bucket for Terraform state"
   type        = string
 }
-variable "aws_region" {
-  description = "The AWS region for resources"
-  type        = string
-}
+# variable "aws_region" {
+#   description = "The AWS region for resources"
+#   type        = string
+# }
