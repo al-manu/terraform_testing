1e7da0bc5c13a59518582cc2ec9b6cc2c1bed739
Ala Mani <ala.a.mani@capgemini.com>
2024-11-07 14:46:47 +0100

    add major updates for lock

--- backend.tf
+++ backend.tf
@@ -3,15 +3,10 @@
 terraform {
   backend "s3" {
     bucket         = var.tf_state_bucket  # S3 bucket to store state
-    key            = "terraform/${var.environment}/terraform.tfstate"  # State file path per environment
+    key            = "terraform/dev/terraform.tfstate"  # State file path per environment
     region         = var.aws_region  # AWS region where the bucket is located
     encrypt        = true  # Enable encryption for the state file
     acl            = "bucket-owner-full-control"  # Grant full control to the bucket owner
     lock_table     = "terraform-lock-table"  # Use the S3 locking mechanism (no need for DynamoDB)
   }
-}
-
-variable "environment" {
-  description = "The environment for the backend (dev, sit, prod)"
-  type        = string
 }
