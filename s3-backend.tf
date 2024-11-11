d70b83f49a0400f0c9961a9158f1cf077debba6b
Ala Mani <ala.a.mani@capgemini.com>
2024-11-11 10:43:43 +0100

    adding features 030

--- s3-backend.tf
+++ s3-backend.tf
@@ -1,22 +1,28 @@
-resource "aws_s3_bucket" "state_bucket" {
-  bucket = "${var.environment}-state-bucket1"
-  acl    = "private"
+# resource "aws_s3_bucket" "state_bucket" {
+#   bucket = "${var.environment}-state-bucket1"
+#   acl    = "private"
+#  lifecycle {
+#     prevent_destroy = true  # Prevents accidental deletion of the bucket
+#   }
+#   tags = {
+#     Name        = "State Bucket"
+#     Environment = var.environment
+#   }
+  
+# }
 
-  tags = {
-    Name        = "State Bucket"
-    Environment = var.environment
-  }
-}
+# resource "aws_s3_bucket" "lock_bucket" {
+#   bucket = "${var.environment}-lock-bucket1"
+#   acl    = "private"
+#    lifecycle {
+#     prevent_destroy = true  # Prevents accidental deletion of the bucket
+#   }
 
-resource "aws_s3_bucket" "lock_bucket" {
-  bucket = "${var.environment}-lock-bucket1"
-  acl    = "private"
-
-  tags = {
-    Name        = "Lock Bucket"
-    Environment = var.environment
-  }
-}
+#   tags = {
+#     Name        = "Lock Bucket"
+#     Environment = var.environment
+#   }
+# }
 
 # output "state_bucket_name" {
 #   value = aws_s3_bucket.state_bucket.bucket
@@ -25,3 +31,55 @@ resource "aws_s3_bucket" "lock_bucket" {
 # output "lock_bucket_name" {
 #   value = aws_s3_bucket.lock_bucket.bucket
 # }
+
+
+# data "aws_s3_bucket" "existing_state" {
+#   bucket = "my-unique-state-bucket"
+# }
+
+# data "aws_s3_bucket" "existing_lock" {
+#   bucket = "my-unique-lock-bucket"
+# }
+
+
+
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = "${var.environment}-state-bucket1"
+  acl    = "private"
+
+  lifecycle {
+    prevent_destroy = true  # Prevents accidental deletion of the bucket
+  }
+
+  tags = {
+    Name        = "State Bucket"
+    Environment = var.environment
+  }
+
+  # Using the data block to check if the bucket already exists
+
+}
+  data "aws_s3_bucket" "existing_state" {
+    bucket = "${var.environment}-state-bucket1"
+  }
+
+resource "aws_s3_bucket" "lock_bucket" {
+  bucket = "${var.environment}-lock-bucket1"
+  acl    = "private"
+
+  lifecycle {
+    prevent_destroy = true  # Prevents accidental deletion of the bucket
+  }
+
+  tags = {
+    Name        = "Lock Bucket"
+    Environment = var.environment
+  }
+
+  # Using the data block to check if the bucket already exists
+
+}
+
+  data "aws_s3_bucket" "existing_lock" {
+    bucket = "${var.environment}-lock-bucket1"
+  }
