ba0357a49f8cb10d364d163d0a07962d71fd6c4d
Ala Mani <ala.a.mani@capgemini.com>
2024-11-11 11:23:32 +0100

    adding features 030

--- s3-backend.tf
+++ s3-backend.tf
@@ -43,45 +43,79 @@
 
 
 
+# resource "aws_s3_bucket" "state_bucket" {
+#   bucket = "${var.environment}-state-bucket1"
+#   count  = length(data.aws_s3_bucket.existing_state.id) == 0 ? 1 : 0  # Create only if it doesn't exist
+#   acl    = "private"
+
+#   lifecycle {
+#     prevent_destroy = true  # Prevents accidental deletion of the bucket
+#   }
+
+#   tags = {
+#     Name        = "State Bucket"
+#     Environment = var.environment
+#   }
+
+#   # Using the data block to check if the bucket already exists
+
+# }
+#   data "aws_s3_bucket" "existing_state" {
+#     bucket = "${var.environment}-state-bucket1"
+#   }
+
+# resource "aws_s3_bucket" "lock_bucket" {
+#   bucket = "${var.environment}-lock-bucket1"
+#   count  = length(data.aws_s3_bucket.existing_lock.id) == 0 ? 1 : 0  # Create only if it doesn't exist
+#   acl    = "private"
+
+#   lifecycle {
+#     prevent_destroy = true  # Prevents accidental deletion of the bucket
+#   }
+
+#   tags = {
+#     Name        = "Lock Bucket"
+#     Environment = var.environment
+#   }
+
+#   # Using the data block to check if the bucket already exists
+
+# }
+
+#   data "aws_s3_bucket" "existing_lock" {
+#     bucket = "${var.environment}-lock-bucket1"
+#   }
+
+
+
+
+
 resource "aws_s3_bucket" "state_bucket" {
+  count = length(data.aws_s3_bucket.existing_state.id) == 0 ? 1 : 0
   bucket = "${var.environment}-state-bucket1"
-  count  = length(data.aws_s3_bucket.existing_state.id) == 0 ? 1 : 0  # Create only if it doesn't exist
   acl    = "private"
 
   lifecycle {
-    prevent_destroy = true  # Prevents accidental deletion of the bucket
+    prevent_destroy = true
   }
 
   tags = {
     Name        = "State Bucket"
     Environment = var.environment
   }
-
-  # Using the data block to check if the bucket already exists
-
 }
-  data "aws_s3_bucket" "existing_state" {
-    bucket = "${var.environment}-state-bucket1"
-  }
 
 resource "aws_s3_bucket" "lock_bucket" {
+  count = length(data.aws_s3_bucket.existing_lock.id) == 0 ? 1 : 0
   bucket = "${var.environment}-lock-bucket1"
-  count  = length(data.aws_s3_bucket.existing_lock.id) == 0 ? 1 : 0  # Create only if it doesn't exist
   acl    = "private"
 
   lifecycle {
-    prevent_destroy = true  # Prevents accidental deletion of the bucket
+    prevent_destroy = true
   }
 
   tags = {
     Name        = "Lock Bucket"
     Environment = var.environment
   }
-
-  # Using the data block to check if the bucket already exists
-
 }
-
-  data "aws_s3_bucket" "existing_lock" {
-    bucket = "${var.environment}-lock-bucket1"
-  }
