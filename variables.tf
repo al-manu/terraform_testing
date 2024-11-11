18371e8870bd91e417ea651682d61d7ad5877de2
Ala Mani <ala.a.mani@capgemini.com>
2024-11-11 09:46:16 +0100

    adding features 33

--- variables.tf
+++ variables.tf
@@ -3,25 +3,25 @@
 #   type        = string
 # }
 
-variable "in_bucket_name" {
-  description = "Name of the input S3 bucket"
-  type        = string
-}
+# variable "in_bucket_name" {
+#   description = "Name of the input S3 bucket"
+#   type        = string
+# }
 
-variable "out_bucket_name" {
-  description = "Name of the output S3 bucket"
-  type        = string
-}
+# variable "out_bucket_name" {
+#   description = "Name of the output S3 bucket"
+#   type        = string
+# }
 
-variable "tmp_bucket_name" {
-  description = "Name of the temporary S3 bucket"
-  type        = string
-}
+# variable "tmp_bucket_name" {
+#   description = "Name of the temporary S3 bucket"
+#   type        = string
+# }
 
-variable "export_bucket_name" {
-  description = "Name of the export S3 bucket"
-  type        = string
-}
+# variable "export_bucket_name" {
+#   description = "Name of the export S3 bucket"
+#   type        = string
+# }
 
 
 # variable "TF_STATE_BUCKET" {
