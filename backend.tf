ae7bbb2aa332862c647256b746b623aa06cba5dc
Ala Mani <ala.a.mani@capgemini.com>
2024-11-11 12:28:06 +0100

    adding features 0

--- backend.tf
+++ backend.tf
@@ -1,15 +1,15 @@
 # backend.tf: Configure S3 as the backend for both state storage and locking
 
-# terraform {
-#   backend "s3" {
-#     bucket         = var.tf_state_bucket  # S3 bucket to store state
-#     key            = "terraform/dev/terraform.tfstate"  # State file path per environment
-#     region         = var.aws_region  # AWS region where the bucket is located
-#     encrypt        = true  # Enable encryption for the state file
-#     acl            = "bucket-owner-full-control"  # Grant full control to the bucket owner
-#     lock_table     = "terraform-lock-table"  # Use the S3 locking mechanism (no need for DynamoDB)
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = var.state_bucket  # S3 bucket to store state
+    key            = "terraform/dev/terraform.tfstate"  # State file path per environment
+    region         = var.aws_region  # AWS region where the bucket is located
+    encrypt        = true  # Enable encryption for the state file
+    acl            = "bucket-owner-full-control"  # Grant full control to the bucket owner
+    lock_table     = var.lock_bucket  # Use the S3 locking mechanism (no need for DynamoDB)
+  }
+}
 
 # terraform {
 #   backend "s3" {
@@ -34,3 +34,11 @@
 # }
 
 
+# terraform {
+#   backend "s3" {
+#     bucket = "my-terraform-state-bucket"
+#     key    = "terraform/dev/dev.tfstate"
+#     region = "us-west-2"
+#     encrypt = true
+#   }
+# }
